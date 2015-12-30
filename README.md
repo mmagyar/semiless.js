469e2f970231e50444378746fb28a7e11e91c469
Mate Magyar <dev@mmagyar.com>
2015-12-30 17:55:00 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # semiless.js
 Forget about the semicolons safely.
+
+I really dislike semicolons, they add a lot of visual noise, with no real benefits, but it's a shame that javascript kind of botched up statement termination.
+But worry not, this tool is here to help with those deficiencies
+
+If you type something like this:
+```
+var myNumber = 32
+(function(text){ console.log(text) })(myNumber)
+```
+you will get en error message, since this would like to invoke 32 as a function :
+
+```
+error in file: /opt/code/mysuperproject/js_source/forms.js
+    2 : (function(text){ console.log(text) })(myNumber), 
+```
+
+### When does an error occures?
+if a line starts with one of these :
+```
+/ + - * ( [ & % | < > = ^
+```
+Of course, an error is only raised if the error is real, for example this will not cause and error:
+```
+if (
+    (cat && dog) &&
+    (turtle && penguin)
+)
+++animalPairsCount
+```
+because the & symbol clearly signals that you want to continue on the next line, since there is no other meaning for symbol without a next statement, after that the double plus signs will close the previous statement
+
+## Usage:
+
+This will recoursively check all folders and files, if the file extension matches
+```javascript
+var semiless = require('semiless')
+var errors = semiless.fileChecker('js_source', [.js, .ts])
+
+if (errors) {
+    console.error(errors)
+} else {
+    console.log('no error found in source files')
+ }
+
+
+```
---
+++ semiless.js
@@ -0,0 +1,170 @@
+/** Copyright 2015 Magyar Mate under MIT */
+var fs   = require('fs');
+var path = require('path');
+
+function checkValidStatementBeginning(fileContent) {
+    var createCheckerState = function () {
+        return {
+            errorLines    : [],
+            inComment     : false,
+            lastLineEnding: ''
+        }
+    }
+
+    var checkerState = createCheckerState()
+
+    var illegalStringStart = function (string) {
+        var first  = string[0]
+        var second = string[1]
+        switch (first) {
+            case '/':
+                return second === '/' ? false : (second !== '*')
+            case '+':
+                return second !== '+';
+            case '-':
+                return second !== '-'
+            case '*':
+                return second !== '/'
+            case '(':
+            case '[':
+            case '&':
+            case '%':
+            case '|':
+            case '<':
+            case '>':
+            case '=':
+            case '^':
+                return true
+            default :
+                return false
+        }
+    }
+
+    var lineStat = function (index, line) {
+        return {
+            index: index,
+            line : line
+
+        }
+    }
+
+
+    var checkLineByCharacter = function (line, checkerState) {
+        var lastEnding              = checkerState.lastLineEnding
+        checkerState.lastLineEnding = line[line.length - 1]
+
+        //We don't need to worry if the preceding character is a comma, semicolon, opening paren|bracket|braces on the last line, since it closes the statement,
+        //Or a character that clearly signals that we want continue, like a plus sing at the end of the line
+        //Also opening a new block clears any pending statements
+        return !!(
+            lastEnding == "," ||
+            lastEnding == ";" ||
+            lastEnding == ":" ||
+            lastEnding == "{" ||
+            illegalStringStart(lastEnding)
+        );
+
+
+    }
+
+    //TODO make the parser recursive, separate every function, and process and innard functions recusively
+
+    var processLine = function (line, i) {
+
+        var lineCommentIndex = line.indexOf('//')
+        var commentOpenIndex = line.indexOf('/*')
+        //You can comment out the start of a comment block with line block
+        if (lineCommentIndex !== -1 && lineCommentIndex < commentOpenIndex) commentOpenIndex = -1
+
+        var commentCloseIndex = line.indexOf('*/')
+        if (commentOpenIndex === 0) checkerState.inComment = true
+
+        if (commentCloseIndex != -1) {
+            checkerState.inComment = false
+            return
+        }
+
+        if ((  checkerState.inComment || lineCommentIndex === 0)) return
+
+
+        if (checkLineByCharacter(line, checkerState)) return
+        if (illegalStringStart(line)) checkerState.errorLines.push(lineStat(i, line))
+        if (commentOpenIndex !== -1) checkerState.inComment = true
+
+    }
+
+
+    fileContent.split('\n').map(function (x) {return x.trim()}).forEach(processLine)
+    return checkerState.errorLines
+
+}
+
+
+var walk = function (dir, done) {
+    var results = [];
+    fs.readdir(dir, function (err, list) {
+        if (err) throw err;
+        var pending = list.length;
+        if (!pending) return done(results);
+        list.forEach(function (file) {
+            file = path.resolve(dir, file);
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function (res) {
+                        results = results.concat(res);
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    results.push(file);
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+};
+
+
+function hasArray(arr) {
+    for (var i = 0, l = arr.length; i < l; i++) if (arr[i] instanceof Array) return true
+    return false
+}
+
+var flatten = function (list) {return list.reduce(function (p, n) {return p.concat(n);}, [])};
+
+var walkSync = function (dir) {
+    return flatten(fs.readdirSync(dir).map(function (x) {return path.resolve(dir, x)})
+                     .map(function (file) {return fs.statSync(file).isDirectory() ? walkSync(file) : file}))
+}
+
+/**
+ *
+ * @param pathToList string
+ * @param extensionsToWatch string[]
+ */
+var walkFilter = function (pathToList, extensionsToWatch) {
+    return walkSync(pathToList).filter(function (x) {return extensionsToWatch.filter(function (y) {return y === path.extname(x)}).length !== 0})
+}
+
+
+/**
+ *
+ * @param folderToCheck string
+ * @param fileExtensionsToCheck string[] the extension of the watched files, must include the dot before extension, like: ['.ts', '.js']
+ * @param filesEncoding defaults to utf8
+ * @returns {*}
+ */
+function fileChecker(folderToCheck, fileExtensionsToCheck, filesEncoding) {
+    var files        = walkFilter(folderToCheck || 'ts', fileExtensionsToCheck || ['.ts', '.js'])
+    var filesContent = files.map(function (x) {return fs.readFileSync(x, filesEncoding || 'utf8')})
+
+    return filesContent
+        .map(checkValidStatementBeginning)
+        .map(function (x, i) {return {fileName: files[i], error: x}})
+        .filter(function (x) { return x.error.length !== 0})
+        .map(function (x) {return "error in file: " + x.fileName + x.error.map(function (y) {return "\n    " + (y.index + 1) + " : " + y.line})})
+        .reduce(function (x, y) {return x + '\n' + y}, "")
+
+
+}
+
+module.exports.fileChecker = fileChecker
